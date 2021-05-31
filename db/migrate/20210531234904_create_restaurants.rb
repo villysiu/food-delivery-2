b18f32f6ac355b5a38cf9c616da39139d72d35bf
villysiu <villysiu@gmail.com>
2021-05-31 16:50:01 -0700

    rails g resource Restaurant

---
+++ db/migrate/20210531234904_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.integer :rating
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
